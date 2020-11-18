ee53ea3a014f22c1ed7584dbaf82511d057d1792
garret <garretcabello@gmail.com>
2020-11-18 17:20:25 -0500

    starting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ObservationsComponent } from './observations/observations.component';
+import { PortraitsComponent } from './portraits/portraits.component';
+import { StudioComponent } from './studio/studio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component:  HomeComponent},
+  { path: 'portraits', component:  PortraitsComponent},
+  { path: 'observations', component:  ObservationsComponent},
+  { path: 'studio', component:  StudioComponent},
+  { path: 'about', component:  AboutComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component:  HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortraitsComponent } from './portraits/portraits.component';
+import { ObservationsComponent } from './observations/observations.component';
+import { StudioComponent } from './studio/studio.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortraitsComponent,
+    ObservationsComponent,
+    StudioComponent,
+    AboutComponent,
+    HeaderComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
